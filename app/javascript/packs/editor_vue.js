64fd9e267369f2aa95e724edb46dac6c2d593b68
sho ishihara <sho.ishihara@theport.jp>
2017-03-25 23:34:00 +0900

    bindingテスト完了

--- app/javascript/packs/editor_vue.js
+++ app/javascript/packs/editor_vue.js
@@ -7,12 +7,52 @@
 import Vue from 'vue/dist/vue.esm'
 import App from './app.vue'
 
+Vue.component('Editor', {
+  template: '<div :id="editorId" style="width: 100%; height: 100%;"></div>',
+  props: ['editorId', 'content'],
+  data () {
+    return {
+      editor: Object,
+      beforeContent: this.content
+    }
+  },
+  mounted () {   
+    this.editor = window.ace.edit(this.editorId)
+    this.editor.setValue(this.content, 1)
+    this.editor.setTheme("ace/theme/monokai");
+    this.editor.getSession().setMode("ace/mode/markdown");
+    this.editor.getSession().setTabSize(2);
+    this.editor.getSession().setUseWrapMode(true);
+ 
+    this.editor.on('change', () => {
+      this.beforeContent = this.editor.getValue()
+      this.$emit('change-content', this.editor.getValue())
+    })
+  }
+})
+
 document.addEventListener('DOMContentLoaded', () => {
-  document.body.appendChild(document.createElement('hello'))
   const app = new Vue({
-    el: 'hello',
-    template: '<App/>',
-    components: { App }
+    el: '#container',
+    data () {
+      return {
+        result: 'ここにプレビューが表示されます'
+      }
+    },
+    components: { App },
+    methods: {
+      apply (val) {
+
+        // if (this.result !== val) {
+        //   $('#pre_article_markdown').val(val)
+        //   this.$http.post('/api/markdowns/to_html', { markdown: val }).then(response => {
+        //     this.result = response.body['html'];
+        //   }, response => {
+        //   });
+        // }
+        this.result = val
+      }
+    }
   })
 
   console.log(app)
