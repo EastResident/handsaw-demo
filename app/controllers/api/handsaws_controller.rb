39f5fe5e94d5164569b501c2a8ac89a455d23636
sho ishihara <sho.ishihara@theport.jp>
2017-03-26 17:25:20 +0900

    一旦完成

--- app/controllers/api/handsaws_controller.rb
+++ app/controllers/api/handsaws_controller.rb
@@ -2,10 +2,12 @@
 require 'json'
 module Api
   class HandsawsController < ApplicationController
+    include ActionView::Helpers::TextHelper
     protect_from_forgery with: :null_session
     def convert
-      html = Handsaw::Processor.new.render(params[:handsaw]).html_safe
-      render json: JSON.generate(html: html)
+      html = Handsaw::Processor.new.render(params[:handsaw])
+      indent_html = Nokogiri::HTML.fragment(html.delete("\n")).to_xhtml(indent: 2)
+      render json: JSON.generate(html: indent_html)
     end
   end
 end
